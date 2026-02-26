360af900d9237c4512e23ff52de2b66b313a43f5
Gokul Selvan <mgokulselvan@gmail.com>
2026-02-26 17:54:41 +0530

    Update board printing to include coordinate labels

--- chess_from_scratch/printBoard.py
+++ chess_from_scratch/printBoard.py
@@ -1,5 +1,15 @@
 def printBoard(board):
-    for row in board:
+    for colGuide in ['a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' ]:
+        print(colGuide,end = " ")
+
+    print()
+
+    for _ in range(8):
+        print("_" , end = " ")
+
+    print()
+
+    for (rowNo,row) in enumerate(board):
         for box in row:
             print(box,end = " ")
-        print()
+        print("|",8-rowNo)
