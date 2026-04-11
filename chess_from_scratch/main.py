7ec03741f4f1911e6577987c1d568cdf560d9252
Gokul Selvan <mgokulselvan@gmail.com>
2026-04-11 16:16:41 +0530

    feat: Handle KeyboardInterrupt to end game, show instructions at game start

--- chess_from_scratch/main.py
+++ chess_from_scratch/main.py
@@ -29,70 +29,90 @@ castlingRights = {
     "black-queenside":True
 }
 
-while(True):
+instructions="""
+CHESS CLI - INSTRUCTIONS
 
-    legalMoves = generateAllLegalMoves(gameBoard , currentTurn , movesHistory,castlingRights)
-    if len(legalMoves) == 0:
+Move format:
+- Use: e2e4 (source → destination)
+- Example: g1f3, e7e5
+
+Rules:
+- Only legal moves allowed
+- Game ends on checkmate or draw
+
+Controls:
+- Ctrl+C to exit
+
+"""""
+print(instructions)
+
+try:
+    while(True):
+
+        legalMoves = generateAllLegalMoves(gameBoard , currentTurn , movesHistory,castlingRights)
+        if len(legalMoves) == 0:
+            if currentTurn == TURN.WHITE:
+                print("Black checkmates White, Black wins")
+            elif currentTurn == TURN.BLACK:
+                print("White checkmates Black, White wins")
+
+            restartFlag = input("Do you want to restart the game?(Y/N)")
+            if restartFlag.lower() == 'y':
+                movesHistory = []
+                gameBoard = copy.deepcopy(StartingBoard)
+                currentTurn = TURN.WHITE
+            elif restartFlag.lower() == 'n':
+                break
+
+        printBoard(gameBoard)
         if currentTurn == TURN.WHITE:
-            print("Black checkmates White, Black wins")
+            print("White Player's turn")
         elif currentTurn == TURN.BLACK:
-            print("White checkmates Black, White wins")
+            print("Black Player's turn")
 
-        restartFlag = input("Do you want to restart the game?(Y/N)")
-        if restartFlag.lower() == 'y':
-            movesHistory = []
-            gameBoard = copy.deepcopy(StartingBoard)
-            currentTurn = TURN.WHITE
-        elif restartFlag.lower() == 'n':
-            break
+        move = input("Enter your move:")
+        if not isValidNotation(move):
+            print("The Inputted move is does not follow the notation, please enter the move again")
+            continue
 
-    printBoard(gameBoard)
-    if currentTurn == TURN.WHITE:
-        print("White Player's turn")
-    elif currentTurn == TURN.BLACK:
-        print("Black Player's turn")
-
-    move = input("Enter your move:")
-    if not isValidNotation(move):
-        print("The Inputted move is does not follow the notation, please enter the move again")
-        continue
-
-    if move not in legalMoves:
-        print("Not a Legal Move, please enter a legal move")
-        continue
-    else:
-
-        #Check if Castling done
-        currentMoveCoords=moves(move)
-
-        if(currentMoveCoords[1]==(0,0)):castlingRights["black-queenside"]=False;
-        if(currentMoveCoords[1]==(0,7)):castlingRights["black-kingside"]=False;
-        if(currentMoveCoords[1]==(7,0)):castlingRights["white-queenside"]=False;
-        if(currentMoveCoords[1]==(7,7)):castlingRights["white-kingside"]=False;
-
-        if(currentTurn==TURN.BLACK):
-
-            if currentMoveCoords[0]==(0,4):#if source is king
-                castlingRights["black-kingside"]=False;
-                castlingRights["black-queenside"]=False;
-            elif currentMoveCoords[0]==(0,7):#source is black rook(king side)
-                castlingRights["black-kingside"]=False;
-            elif currentMoveCoords[0]==(0,0):#source is black rook(queenside side)
-                castlingRights["black-queenside"]=False;
-
-            currentTurn=TURN.WHITE
+        if move not in legalMoves:
+            print("Not a Legal Move, please enter a legal move")
+            continue
         else:
 
-            if currentMoveCoords[0]==(7,4):#if source is king
-                castlingRights["white-kingside"]=False;
-                castlingRights["white-queenside"]=False;
-            elif currentMoveCoords[0]==(7,7):#source is black rook(king side)
-                castlingRights["white-kingside"]=False;
-            elif currentMoveCoords[0]==(7,0):#source is black rook(queenside side)
-                castlingRights["white-queenside"]=False;
+            #Check if Castling done
+            currentMoveCoords=moves(move)
 
-            currentTurn=TURN.BLACK
+            if(currentMoveCoords[1]==(0,0)):castlingRights["black-queenside"]=False;
+            if(currentMoveCoords[1]==(0,7)):castlingRights["black-kingside"]=False;
+            if(currentMoveCoords[1]==(7,0)):castlingRights["white-queenside"]=False;
+            if(currentMoveCoords[1]==(7,7)):castlingRights["white-kingside"]=False;
 
-        gameBoard = makeMove(gameBoard , move)
+            if(currentTurn==TURN.BLACK):
 
-        movesHistory.append(move)
+                if currentMoveCoords[0]==(0,4):#if source is king
+                    castlingRights["black-kingside"]=False;
+                    castlingRights["black-queenside"]=False;
+                elif currentMoveCoords[0]==(0,7):#source is black rook(king side)
+                    castlingRights["black-kingside"]=False;
+                elif currentMoveCoords[0]==(0,0):#source is black rook(queenside side)
+                    castlingRights["black-queenside"]=False;
+
+                currentTurn=TURN.WHITE
+            else:
+
+                if currentMoveCoords[0]==(7,4):#if source is king
+                    castlingRights["white-kingside"]=False;
+                    castlingRights["white-queenside"]=False;
+                elif currentMoveCoords[0]==(7,7):#source is black rook(king side)
+                    castlingRights["white-kingside"]=False;
+                elif currentMoveCoords[0]==(7,0):#source is black rook(queenside side)
+                    castlingRights["white-queenside"]=False;
+
+                currentTurn=TURN.BLACK
+
+            gameBoard = makeMove(gameBoard , move)
+
+            movesHistory.append(move)
+except KeyboardInterrupt:
+    print("\nGame Exiting...")
