d44b4fb01b7f0946755129c7963fd05d312c4a80
Gokul Selvan <mgokulselvan@gmail.com>
2026-09-06 10:13:31 +0530

    moves include pawn promotion
    looks buggy, have to add more compute shennanigans to make it "feel" premium, like when the popup appears for pawn promotion even for invalid moves, but when you do actually select a piece, it doesnt actually promote, (legality of move is being checked after generation of the move, and the chosing of the promotion piece is during generation of the move , so....) have to just do tiny bit more work , which i ll do later, cuz im LAZY , and it doesnt actually make the game wrong

--- gui/gui.py
+++ gui/gui.py
@@ -31,20 +31,19 @@ StartingCastlingRights = {
 overlay = pygame.Surface((boxlen, boxlen), pygame.SRCALPHA)
 overlay.fill((255, 165, 0, 150))
 
+#popup for when player has to promote a pawn
+popup = pygame.Surface((400,200),pygame.SRCALPHA)
+popup.fill((30,30,30,230))
+
 def getBoardPos(coords):
     (x , y) = coords
     rowNo = x//80
     colNo = y//80
     return (rowNo,colNo)
 
-def boardPosToMove(firstPos,secPos):
-    move=''
-    indexToAlphaMap = ["a","b","c","d","e","f","g","h"]
-    indexToNumMap = ["8","7","6","5","4","3","2","1"]
-    move+=(indexToAlphaMap[firstPos[0]]+indexToNumMap[firstPos[1]]+indexToAlphaMap[secPos[0]]+indexToNumMap[secPos[1]])
-    return move
 
-def drawBoard(screen,board):
+def drawBoard(screen,board,piecePos,firstClickDone):
+    screen.fill(bgcol)
     #Drawing the board with 8x8 boxes
     for rowno,row in enumerate(board): #rowno is y , and colno is x
         for colno,box in enumerate(row):
@@ -63,7 +62,64 @@ def drawBoard(screen,board):
                 piece=piece+box.lower()
                 screen.blit(p[piece], (colno * boxlen+5, rowno * boxlen+5))
 
+    #highlighting the piece that the mouse clicks
+    if firstClickDone:
+        screen.blit(overlay,(piecePos[0] * boxlen, piecePos[1] * boxlen))
 
+
+def drawPromotionMenu(screen, color):
+    screen.blit(popup, (120, 220))
+
+    pieces = ["q", "r", "b", "n"]
+
+    for i, piece in enumerate(pieces):
+        screen.blit(
+            p[color + piece],
+            (130 + i * 90, 285)
+        )
+
+
+def boardPosToMove(firstPos,secPos,screen,board,firstClickDone,running,currentTurn,clock):
+    move=''
+    indexToAlphaMap = ["a","b","c","d","e","f","g","h"]
+    indexToNumMap = ["8","7","6","5","4","3","2","1"]
+    move+=(indexToAlphaMap[firstPos[0]]+indexToNumMap[firstPos[1]]+indexToAlphaMap[secPos[0]]+indexToNumMap[secPos[1]])
+    #checking if the move is a pawn promotion
+    if( (firstPos[1]==1 and secPos[1]==0 and board[firstPos[1]][firstPos[0]]=='P' ) or (firstPos[1]==6 and secPos[1]==7 and board[firstPos[1]][firstPos[0]]=='p') ):
+        notchosen = True
+        turnstr = "w" if currentTurn == TURN.WHITE else "b"
+        while notchosen:
+            drawBoard(screen,board,firstPos,firstClickDone)
+            drawPromotionMenu(screen,turnstr)
+
+            for event in pygame.event.get():
+                if event == pygame.QUIT:
+                    notchosen = False
+                    running = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    chosenPiece = ""
+                    mouseX, mouseY = pygame.mouse.get_pos()
+
+                    if 285 <= mouseY <= 355:
+                        if 130 <= mouseX < 200:
+                            chosenPiece = "q"
+                        elif 220 <= mouseX < 290:
+                            chosenPiece = "r"
+                        elif 310 <= mouseX < 380:
+                            chosenPiece = "b"
+                        elif 400 <= mouseX < 470:
+                            chosenPiece = "n"
+                    if chosenPiece !="":
+                        notchosen = False
+                        move+=chosenPiece.upper() if currentTurn == TURN.WHITE else chosenPiece
+
+            pygame.display.flip()#to put all the drawings from above on the screen
+
+            dt = clock.tick(60)/1000#Limit FPS to 60
+
+
+    return move
 
 
 def startGUI():
@@ -104,18 +160,15 @@ def startGUI():
                 else:
                     firstClickDone = False
                     destPos = getBoardPos(pygame.mouse.get_pos())
-                    move = boardPosToMove(piecePos,destPos)
+                    move = boardPosToMove(piecePos,destPos,screen,gameBoard,firstClickDone,running,currentTurn,clock)
+                    print(move)
                     if move in legalMoves:
                         gameBoard = makeMove(gameBoard , move)
                         currentTurn = TURN.BLACK if currentTurn == TURN.WHITE else TURN.WHITE
                         movesHistory.append(move)
 
         #-----------------------drawing----------------------------
-        screen.fill(bgcol)
-        drawBoard(screen,gameBoard)
-        #highlighting the piece that the mouse clicks
-        if firstClickDone:
-            screen.blit(overlay,(piecePos[0] * boxlen, piecePos[1] * boxlen))
+        drawBoard(screen,gameBoard,piecePos,firstClickDone)
         pygame.display.flip()#to put all the drawings from above on the screen
         #----------------------end of drawing-----------------------
 
